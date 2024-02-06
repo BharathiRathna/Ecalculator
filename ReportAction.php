910ce92b5002566f8ab8471beca728ffdcbf1caf
BharathiRathna <156513298+BharathiRathna@users.noreply.github.com>
2024-02-06 18:41:04 +0530

    I have created form for report

---
+++ ReportAction.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'config/Database.php';
+include_once 'class/Report.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$report = new Report($db);
+
+if(!empty($_POST['action']) && $_POST['action'] == 'getReport')
+{
+    $report->fromDate = $_POST['fromDate'];
+    $report->toDate = $_POST['toDate'];
+    $report->getReports();
+}
+    ?>
