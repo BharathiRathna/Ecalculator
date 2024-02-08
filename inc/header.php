e7ceb8e483f86b8385bbfa258d1063b03dbfcb2e
BharathiRathna <156513298+BharathiRathna@users.noreply.github.com>
2024-02-08 18:09:24 +0530

    This branch have common file for the E-Calculator

--- inc/header.php
+++ inc/header.php
@@ -15,5 +15,4 @@
 
 <!--JS Bootstrap link -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
 </head>
--- js/general.js
+++ js/general.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
 	var urlPath = window.location.pathname,
     urlPathArray = urlPath.split('.'),
     tabId = urlPathArray[0].split('/').pop();
