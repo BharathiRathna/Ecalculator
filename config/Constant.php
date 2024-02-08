4575ca55bcb4f25a2af20e8284591e606390fa7c
BharathiRathna <156513298+BharathiRathna@users.noreply.github.com>
2024-02-08 18:12:35 +0530

    Test

--- config/Constant.php
+++ config/Constant.php
@@ -3,4 +3,5 @@ define('HOST','localhost');
 define('USER','root');
 define('PASSWORD','');
 define('DATABASE','e-calculator');
+
 ?>
--- inc/header.php
+++ inc/header.php
@@ -15,4 +15,5 @@
 
 <!--JS Bootstrap link -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 </head>
